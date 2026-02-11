c833f3fa2c445a0390103711662954617351ad45
milankokovac <arondehram@gmail.com>
2026-02-11 10:09:28 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-# Konfigurácia
-BASE_URL = "https://mobil.bazos.sk/{page}?hledat=5g&Submit=H%C4%BEada%C5%A5&rubriky=mobil&category=0&hlokalita=81101&humkreis=200&cenaod=30&cenado=5000&order=&crp=&kitx=ano"
+# Konfiguracia
+BASE_URL = "https://mobil.bazos.sk/{page}?hledat=5g&Submit=H%C4%BEada%C5%A5&rubriky=mobil&category=0&hlokalita=81101&humkreis=125&cenaod=30&cenado=5000&order=&crp=&kitx=ano"
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 DATA_FILE = "videne_inzeraty.txt"
@@ -21,7 +21,6 @@ def scrape():
 
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/91.0.4472.124'}
     
-    # Prechádzame prvé 3 strany (0, 20, 40)
     for start in [0, 20, 40]:
         page_param = f"{start}/" if start > 0 else ""
         current_url = BASE_URL.format(page=page_param)
@@ -36,7 +35,8 @@ def scrape():
             
             title = link_tag.text
             url = "https://www.bazos.sk" + link_tag['href']
-            cena = inz.find('div', class_='listicena').text
+            price_div = inz.find('div', class_='listicena')
+            cena = price_div.text if price_div else "N/A"
             inz_id = url.split('/')[-2]
 
             if inz_id not in videne:
